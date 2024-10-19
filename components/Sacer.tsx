f8da20bfbd92ce6088ecfdcb2aede5959d574b2f
Cypherfunk <andrescarlos2211@gmail.com>
2024-10-18 21:13:27 -0300

    pan con azimuth maximo

--- components/Sacer.tsx
+++ components/Sacer.tsx
@@ -1,38 +1,45 @@
-import React from "react";
+import React, { Suspense, useState } from "react";
 import { Gltf } from "@react-three/drei/native";
-import { Text, StyleSheet, View, TouchableOpacity } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { Suspense, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Canvas } from "@react-three/fiber/native";
-// Te aseguras que el componente sea compatible con Expo
-import useControls from "r3f-native-orbitcontrols";
 import { OrbitControls } from "@react-three/drei";
-import { router } from "expo-router";
 import Loader from "@/components/Loader";
 import Trigger from "@/components/Trigger";
 import sacerModel from "../assets/models/sacer.glb";
 
 export const Sacer = () => {
   const [loading, setLoading] = useState<boolean>(true);
-  const [orbitControls, events] = useControls();
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar animated style="light" />
 
-      <View style={styles.modelContainer} {...events}>
+      <View style={styles.modelContainer}>
         {loading && <Loader />}
         <Canvas>
-          <OrbitControls enablePan={false} enableZoom={false} />
+          {/* Control de la cámara y rotación */}
+          <OrbitControls
+            enablePan={true}  // Puedes habilitar o deshabilitar el paneo
+            enableZoom={true} // Controlar el zoom
+            autoRotate={false} // Si deseas que rote automáticamente, ajusta esto
+            maxPolarAngle={Math.PI / 2} // Limita el ángulo de rotación vertical
+            minPolarAngle={Math.PI / 3} // Ajusta el ángulo mínimo
+            minAzimuthAngle={-Math.PI / 5} // Limitar rotación horizontal hacia la izquierda
+            maxAzimuthAngle={Math.PI / 6} // Limitar rotación horizontal hacia la derecha
+          />
+          {/* Luces para iluminar el modelo */}
           <directionalLight position={[1, 0, 0]} args={["white", 2]} />
           <directionalLight position={[-1, 0, 0]} args={["white", 2]} />
           <directionalLight position={[0, 0, 1]} args={["white", 2]} />
           <directionalLight position={[0, 0, -1]} args={["white", 2]} />
           <directionalLight position={[0, 1, 0]} args={["white", 15]} />
           <directionalLight position={[0, -1, 0]} args={["white", 2]} />
+          {/* Cargar el modelo 3D */}
           <Suspense fallback={<Trigger setLoading={setLoading} />}>
             <group>
-              <Gltf src={sacerModel} scale={3.2} />
+              <Gltf src={sacerModel} scale={3} />
             </group>
           </Suspense>
         </Canvas>
@@ -47,14 +54,13 @@ export const Sacer = () => {
           todo", está rodeada de misterios, libros y lunas, porque, claro, ella
           es la reina del drama astrológico. Su especialidad es hacerte sentir
           que tu intuición es un súper poder y que lo más cool del mundo es no
-          contarle nada a nadie. ¿Sabes ese momento en el que algo te dice que
-          debes seguir tu instinto? Bueno, es ella mandándote un WhatsApp mental
-          desde el más allá.
+          contarle nada a nadie.
         </Text>
       </View>
     </SafeAreaView>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -65,19 +71,6 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: "white",
     textAlign: "center",
-  },
-  button: {
-    backgroundColor: "white",
-    padding: 14,
-    margin: 20,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 8,
-  },
-  textButton: {
-    fontFamily: "Inter-Bold",
-    fontSize: 18,
-    color: "black",
   },
   modelContainer: {
     flex: 1,
