236427570b6ab0dc438a9fb57abcb4bc40e57dae
Cypherfunk <andrescarlos2211@gmail.com>
2024-11-04 16:34:09 -0300

    selector de cartas con animacion

--- app/(tabs)/arcanos.tsx
+++ app/(tabs)/arcanos.tsx
@@ -67,7 +67,6 @@ const Arcanos: React.FC = () => {
       >
         <Suspense fallback={<Text>Loading...</Text>}>
           <Card />
-
         </Suspense>
       </Modal>
     </SafeAreaView>
--- app/(tabs)/tirada.tsx
+++ app/(tabs)/tirada.tsx
@@ -2,15 +2,21 @@ import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import CardBack from "../../assets/images/miniaturas/back.jpg";
 import { Image } from "expo-image";
-import { View, StyleSheet, TouchableOpacity } from "react-native";
+import { View, StyleSheet, Pressable } from "react-native";
 import { Accelerometer } from "expo-sensors";
 import BotonMenuPrincipal from "@/components/BotonMenuPrincipal";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
 
 const Tirada = () => {
   // Parámetros y hooks para el acelerómetro
   const [{ x, y, z }, setData] = useState({ x: 0, y: 0, z: 0 });
   const [subscription, setSubscription] =
     useState<Accelerometer.Subscription | null>(null);
+
   const [lastShakeTime, setLastShakeTime] = useState(Date.now());
 
   const _subscribe = () => {
@@ -45,25 +51,46 @@ const Tirada = () => {
     return () => _unsubscribe();
   }, []);
 
-  const selectCardHandler = () => {
-    // Implementa la lógica para seleccionar una carta
-  } 
-
-  const [shake, useShake] = useState();
-  // implementar funcion de agitacion
   return (
     <SafeAreaView style={styles.container}>
-      {new Array(22).fill(null).map((_, index) => (
-        <TouchableOpacity
-          key={index}
-          style={[styles.card, { transform: [{ translateX: index * 10 }] }]}
-          onPress={() => {
-            console.log("Tocaste un boton");
-          }}
-        >
-          <Image source={CardBack} contentFit="cover" style={styles.image} />
-        </TouchableOpacity>
-      ))}
+      {new Array(22).fill(null).map((_, index) => {
+        const translateY = useSharedValue(0);
+        const [isSelected, setIsSelected] = useState(false);
+
+        const selectCardHandler = () => {
+          setIsSelected(!isSelected);
+          if (isSelected) {
+            translateY.value = withTiming(0, { duration: 500 });
+          } else {
+            translateY.value = withTiming(-100, { duration: 500 });
+          }
+        };
+        const animatedCardStyle = useAnimatedStyle(() => {
+          return {
+            transform: [{ translateY: translateY.value }],
+            margin: index * 10,
+          };
+        });
+
+        return (
+          <Animated.View
+            key={index}
+            style={[
+              styles.card,
+              animatedCardStyle,
+              { transform: [{ translateX: index * 10 }] },
+            ]}
+          >
+            <Pressable onPress={() => selectCardHandler(translateY)}>
+              <Image
+                source={CardBack}
+                contentFit="cover"
+                style={styles.image}
+              />
+            </Pressable>
+          </Animated.View>
+        );
+      })}
       <View style={styles.boton}>
         <BotonMenuPrincipal nombre="Revolver" ruta="gola" />
       </View>
